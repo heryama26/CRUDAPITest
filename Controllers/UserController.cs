2fa1afbb65891a25ee0784983c8887f30e95e61d
heryana.setiawan <heryanakasih@gmail.com>
2023-12-11 15:27:10 +0700

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using CRUDAPITest.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPITest.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public UserController(MyDbContext context)
+        {
+            _context = context;
+        }
+        [Route("api/getDataUser/{userid}")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserModel>>> getDataUser(string userid)
+        {
+            List<UserModel> userItem = new List<UserModel>();
+            if (userid.ToLower() == "all")
+            {
+                userItem = await _context.tbl_user.ToListAsync();
+
+                if (userItem == null || userItem.Count == 0)
+                {
+                    return NotFound();
+                }
+                return userItem;
+            }
+            else
+            {
+                var getDataById = await _context.tbl_user.FindAsync(int.Parse(userid));
+                if (getDataById == null)
+                {
+                    return NotFound();
+                }
+                userItem.Add(getDataById);
+                return userItem;
+            }
+        }
+        [Route("api/setDataUser")]
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> setDataUser(UserModel user)
+        {
+            _context.tbl_user.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(getDataUser), new { userid = user.userid }, user);
+        }
+        [Route("api/delDataUser/{userid}")]
+        [HttpDelete]
+        public async Task<IActionResult> delDataUser(int userid)
+        {
+            var user = await _context.tbl_user.FindAsync(userid);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.tbl_user.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Model/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAPITest.Model
+{
+    public class MyDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public MyDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                // connect to sql server with connection string from app settings
+                options.UseSqlServer(Configuration.GetConnectionString("ConsString"));
+            }
+        }
+        public DbSet<UserModel> tbl_user { get; set; } = null!;
+    }
+    public class UserModel
+    {
+        [Key]
+        public int userid { get; set; }
+        public string namalengkap { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public char status { get; set; }
+    }
+}
